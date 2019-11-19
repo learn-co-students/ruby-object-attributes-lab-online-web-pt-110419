7116d5377d3b5eacfd8fd2cf856e2a6ede6337c2
Angelica  Thompson <i.thompson89@outlook.com>
2019-11-19 23:59:35 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,18 @@
 class Person
-def initialize(name)
-   @name = name
- end
- 
- def name
-   @name
- end
+def name=(persons_name)
+    @name= persons_name
+  end 
+  
+  def name
+    @name 
+  end
+  
+def job= (persons_job)
+@job = persons_job
+end 
+
+def job
+  @job
+end 
 
 end 
