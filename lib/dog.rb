9048126bec3292727d1ba0c90dc8e1f9d67bb557
Alfredo Diaz <mail4alfredo@gmail.com>
2019-12-05 04:49:19 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+  def name
+    @name
+  end
+  
+  def name=(name)
+    @name=name
+  end
+  
+# Beagle = Dog.breed
+  def breed
+    @breed
+  end
+  
+  def breed=(breed)
+    @breed=breed
+  end
+  
+  # fido = Dog.name
+  
+end
