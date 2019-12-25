ba598382c7ac0fecf555f80a4998d22ede9aac8f
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-12-25 09:49:07 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,26 @@
+class Person
+  #def initialize(first_name, last_name, job)
+   # @first_name = first_name
+	  #@last_name = last_name
+	  #@job = job_description
+  #end 
+  
+  def name=(name)
+   
+    @name = name
+    
+     
+  end
+  
+  def name
+   @name
+  end
+  
+  def job=(job_description)
+    @job = job_description
+  end
+  
+  def job 
+    @job 
+  end
+end 
